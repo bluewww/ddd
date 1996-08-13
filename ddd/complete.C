acdfa2f324c16c42ee4809b3cb1d10084c83d091
zeller <zeller@1d73a310-8f1f-0410-b01a-dab0232e19a9>
1996-08-13 11:18:01 +0000

    Fix: use generic print command

--- ddd/complete.C
+++ ddd/complete.C
@@ -406,7 +406,7 @@ static void _complete_argAct(Widget w,
     }
 
     // The command to use as prefix for completions
-    string base = "print";
+    string base = gdb->print_command();
     if (*num_args >= 1)
 	base = args[0];
 
