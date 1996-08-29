e4cf38d1fe965243134aa41713e14ac064948b98
zeller <zeller@1d73a310-8f1f-0410-b01a-dab0232e19a9>
1996-08-29 07:53:22 +0000

    Sparc fixes

--- libiberty/alloca-norm.h
+++ libiberty/alloca-norm.h
@@ -1,17 +1,27 @@
 /* "Normal" configuration for alloca.  */
 
 #ifdef __GNUC__
+
 #define alloca __builtin_alloca
+
 #else /* not __GNUC__ */
+
 #ifdef sparc
 #include <alloca.h>
 /* Stupid include file doesn't declare it */
-extern PTR __builtin_alloca();
+#ifdef __STDC__
+extern PTR __builtin_alloca(size_t);
 #else
+extern PTR __builtin_alloca();
+#endif
+
+#else /* not sparc */
+
 #ifdef __STDC__
 PTR alloca (size_t);
 #else
 PTR alloca ();			/* must agree with functions.def */
 #endif
-#endif /* sparc */
+
+#endif /* not sparc */
 #endif /* not __GNUC__ */
