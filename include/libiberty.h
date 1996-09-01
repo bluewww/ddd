05474f593235e81399813a2cc023ad6bb9aa2123
zeller <zeller@1d73a310-8f1f-0410-b01a-dab0232e19a9>
1996-09-01 20:27:22 +0000

    Fix: provide correct basename() signature

--- include/libiberty.h
+++ include/libiberty.h
@@ -23,7 +23,7 @@ extern void freeargv PARAMS ((char **));
 
 /* Return the last component of a path name.  */
 
-extern char *basename ();
+extern char *basename PARAMS ((const char *));
 
 /* Concatenate an arbitrary number of strings, up to (char *) NULL.
    Allocates memory using xmalloc.  */
