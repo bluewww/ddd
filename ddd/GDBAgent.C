e7fe498f00e75c2f969c3f3ef5364ff317544b05
zeller <zeller@1d73a310-8f1f-0410-b01a-dab0232e19a9>
1997-12-23 22:57:13 +0000

    The 12 bugs of christmas

--- ddd/GDBAgent.C
+++ ddd/GDBAgent.C
@@ -29,6 +29,115 @@
 char GDBAgent_rcsid[] =
     "$Id$";
 
+// From: hobson@pipper.enet.dec.com
+// Subject: And the Twelve Bugs of Christmas ....
+// Keywords: topical, heard it, computers, chuckle, originally
+//           appeared in fourth quarter, 1992
+// Newsgroups: rec.humor.funny.reruns
+// Date: Tue, 23 Dec 97 13:20:02 EST
+// 
+// For the first bug of Christmas, my manager said to me
+//      See if they can do it again.
+// 
+// For the second bug of Christmas, my manager said to me
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the third bug of Christmas, my manager said to me
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the fourth bug of Christmas, my manager said to me
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the fifth bug of Christmas, my manager said to me
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the sixth bug of Christmas, my manager said to me
+//      Reinstall the software
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the seventh bug of Christmas, my manager said to me
+//      Say they need an upgrade
+//      Reinstall the software
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the eighth bug of Christmas, my manager said to me
+//      Find a way around it
+//      Say they need an upgrade
+//      Reinstall the software
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the ninth bug of Christmas, my manager said to me
+//      Blame it on the hardware
+//      Find a way around it
+//      Say they need an upgrade
+//      Reinstall the software
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the tenth bug of Christmas, my manager said to me
+//      Change the documentation
+//      Blame it on the hardware
+//      Find a way around it
+//      Say they need an upgrade
+//      Reinstall the software
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the eleventh bug of Christmas, my manager said to me
+//      Say it's not supported
+//      Change the documentation
+//      Blame it on the hardware
+//      Find a way around it
+//      Say they need an upgrade
+//      Reinstall the software
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+// 
+// For the twelfth bug of Christmas, my manager said to me
+//      Tell them it's a feature
+//      Say it's not supported
+//      Change the documentation
+//      Blame it on the hardware
+//      Find a way around it
+//      Say they need an upgrade
+//      Reinstall the software
+//      Ask for a dump
+//      Run with the debugger
+//      Try to reproduce it
+//      Ask them how they did it and
+//      See if they can do it again.
+
 #ifdef __GNUG__
 #pragma implementation
 #endif
