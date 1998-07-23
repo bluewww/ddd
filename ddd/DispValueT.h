e0675cae63d313a2283da6aeab0054a946d4e1a9
zeller <zeller@1d73a310-8f1f-0410-b01a-dab0232e19a9>
1998-07-23 18:50:51 +0000

    New: moved `DispValueType' def in separate header file

---
+++ ddd/DispValueT.h
@@ -0,0 +1,54 @@
+// $Id$ -*- C++ -*-
+// DispValue Types
+
+// Copyright (C) 1998 Technische Universitaet Braunschweig, Germany.
+// Written by Andreas Zeller <zeller@ips.cs.tu-bs.de>.
+// 
+// This file is part of DDD.
+// 
+// DDD is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// DDD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with DDD -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// 
+// DDD is the data display debugger.
+// For details, see the DDD World-Wide-Web page, 
+// `http://www.cs.tu-bs.de/softech/ddd/',
+// or send a mail to the DDD developers <ddd@ips.cs.tu-bs.de>.
+
+#ifndef _DDD_DispValueType_h
+#define _DDD_DispValueType_h
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include <iostream.h>
+
+// Various types of elements that can be read
+enum DispValueType {
+    UnknownType = 0,		// Unknown type
+    Simple = 1,			// Ordinary or other value
+    Pointer,			// Pointer value
+    Array,			// Array
+    Struct,		        // Struct, Class, Record, Object
+    Reference,			// C++ Reference.  2 children.
+    Sequence,			// Sequence of values.
+    List,			// List of values.  Last member may be text.
+    Text			// Multi-line text.  Last member of a list.
+};
+
+ostream& operator<<(ostream& os, DispValueType type);
+
+#endif // _DDD_DispValueType_h
+// DON'T ADD ANYTHING BEHIND THIS #endif
