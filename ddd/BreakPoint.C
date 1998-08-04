5e037d5950e3faa008265c1dcff2c24fc18b4bb2
zeller <zeller@1d73a310-8f1f-0410-b01a-dab0232e19a9>
1998-08-04 15:50:26 +0000

    Fix: revert to old behaviour

--- ddd/BreakPoint.C
+++ ddd/BreakPoint.C
@@ -839,21 +839,7 @@ bool BreakPoint::get_state(ostream& os, int nr, bool as_dummy,
 	    }
 	    else
 	    {
-		string file = pos.before(':');
-
-		// Hobi <hobelsbe@forwiss.uni-passau.de> reports:
-		// Solaris DBX uses auxiliary files `FILE.edit.SUFFIX'
-		// and `FILE.gen.SUFFIX'.  DBX requires that
-		// `FILE.SUFFIX' be loaded before these auxiliaries.
-		if (file.contains(".edit.") || file.contains(".gen."))
-		{
-		    string original_file = file;
-		    original_file.gsub(".edit.", ".");
-		    original_file.gsub(".gen.", ".");
-		    os << "file " << original_file << "\n";
-		}
-
-		os << "file "    << file << "\n";
+		os << "file "    << pos.before(':') << "\n";
 		os << "stop at " << pos.after(':')  << cond_suffix << "\n";
 	    }
 	    break;
