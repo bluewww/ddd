e107c974dec458da94b5044a081f36a49764d460
andreas <andreas@1d73a310-8f1f-0410-b01a-dab0232e19a9>
1999-10-01 07:27:02 +0000

    Fix: avoid `unused' warning on `data'

--- ddd/cxxtest.C
+++ ddd/cxxtest.C
@@ -335,6 +335,7 @@ void reference_test(Date& date, Date*& date_ptr)
 void string_test()
 {
     char data[]="one plus two = three";	// Display this
+    (void) data;		// Use it
 }
 
 //--------------------------------------------------------------------------
