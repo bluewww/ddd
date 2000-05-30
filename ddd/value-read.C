240d15dfefde43233ec3822c12c3480756e78b50
andreas <andreas@1d73a310-8f1f-0410-b01a-dab0232e19a9>
2000-05-30 12:49:33 +0000

    New: support Perl hash keys with spaces

--- ddd/value-read.C
+++ ddd/value-read.C
@@ -909,11 +909,25 @@ string read_member_name (string& value, bool picky)
 	    member_name = m;
     }
 
-    if (gdb->type() == PERL && 
-	member_name.contains('\'', 0) && member_name.contains('\'', -1))
+    if (gdb->type() == PERL)
     {
-	// Some Perl debugger flavours quote the member name.
-	member_name = unquote(member_name);
+        if (member_name.contains('\'', 0) && member_name.contains('\'', -1))
+        {
+	    // Some Perl debugger flavours quote the member name.
+	    member_name = unquote(member_name);
+        }
+
+	// A PERL hash key can contain spaces. If we try to strip the
+	// qualifiers, the key name will be the word after the last space
+	// in the key name:
+	// 'my key' will be returned as 'key'
+	// This has several effects:
+	// 1. The key name is displayed incorrectly
+	// 2. Since the key name is wrong, you can't access the associated
+	//    value trough the Display Data window.
+	// 3. Could lead to display a value associated to a valid key
+	// so, turn it OFF.
+	strip_qualifiers = false;
     }
 
     if (member_name.contains("[]", -1))
