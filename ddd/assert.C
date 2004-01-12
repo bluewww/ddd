d287a8cf79e16f184c3dbf9bdea488d44e59273e
arnaud02 <arnaud02@1d73a310-8f1f-0410-b01a-dab0232e19a9>
2004-01-12 13:28:05 +0000

    Add Assert.C. "assert" calls now ddd_assert_fail(). I fixes a build problem where assert is used within <iostream> and causes a build failure. It should produce a smaller executable when assert is enabled.

--- ddd/assert.C
+++ ddd/assert.C
@@ -1,62 +1,40 @@
-// $Id$
-// own assert() macros
-// built on libg++-assert.h and making sure top-level abort() is called
+// $Id$ -*- C++ -*-
+// Function for custom implementation of assert.
 
-// Copyright (C) 1995-1998 Technische Universitaet Braunschweig, Germany.
 // Copyright (C) 2004 Free Software Foundation, USA.
-// Written by Andreas Zeller <zeller@gnu.org>.
-// 
+// Written by Arnaud Desitter.
+//
 // This file is part of DDD.
-// 
+//
 // DDD is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation; either
 // version 2 of the License, or (at your option) any later version.
-// 
+//
 // DDD is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public
 // License along with DDD -- see the file COPYING.
 // If not, write to the Free Software Foundation, Inc.,
 // 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-// 
+//
 // DDD is the data display debugger.
-// For details, see the DDD World-Wide-Web page, 
+// For details, see the DDD World-Wide-Web page,
 // `http://www.gnu.org/software/ddd/',
 // or send a mail to the DDD developers <ddd@gnu.org>.
 
-// Allow this file to be included multiple times with different
-// settings of NDEBUG.
+#include <iostream>
+#include <stdlib.h>
+#include "assert.h"
 
-#if NDEBUG
-#undef assert
-#undef _assert_fn
-#define assert(ignore)
-
-#else  // !NDEBUG
-
-#if HAVE_CONFIG_H
-#include "config.h"		// HAVE_PRETTY_FUNCTION
-#endif
-
-#include "attribute.h"
 void ddd_assert_fail (const char *assertion, const char *file,
-		      unsigned int line, const char *function)
-     ATTRIBUTE_NORETURN;
-
-#undef assert
-#undef _assert_fn
-
-#if HAVE_PRETTY_FUNCTION
-#define _assert_fn __PRETTY_FUNCTION__
-#else // !HAVE_PRETTY_FUNCTION
-#define _assert_fn ""
-#endif // !HAVE_PRETTY_FUNCTION
-
-#define assert(ex) \
-((ex) ? 1 : (ddd_assert_fail (#ex, __FILE__, __LINE__, _assert_fn), 0))
-
-#endif // !NDEBUG
+		      unsigned int line, const char *function) {
+  if ( function != NULL )
+    std::cerr << "\n" << file << ": In function `" << function << "':";
+  std::cerr << "\n" << file << ":" << line
+	    << ": assertion `" << assertion << "' failed\n";
+  ::abort();
+}
