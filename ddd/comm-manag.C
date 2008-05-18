84594b9b39430d1c8221269dd640aa90a67cf21e
pwainwright <pwainwright@1d73a310-8f1f-0410-b01a-dab0232e19a9>
2008-05-18 11:56:42 +0000

    [Patch 6772:6773 from trunk]
    Better with GNU breakpoints.
    macros.texi: update date.

--- ddd/comm-manag.C
+++ ddd/comm-manag.C
@@ -728,13 +728,22 @@ void start_gdb(bool config)
 
     case BASH:
     case PERL:
-    case MAKE:
 	// Bash, Make and Perl start immediately with execution.
 	cmd_data->new_exec_pos = true;
 
 	cmds += gdb->pwd_command();
 	extra_data->refresh_pwd = true;
 	cmds += "L";
+	extra_data->refresh_breakpoints = true;
+	break;
+
+    case MAKE:
+	// Bash, Make and Perl start immediately with execution.
+	cmd_data->new_exec_pos = true;
+
+	cmds += gdb->pwd_command();
+	extra_data->refresh_pwd = true;
+	cmds += "info break";
 	extra_data->refresh_breakpoints = true;
 	break;
 
@@ -1879,12 +1888,28 @@ void send_gdb_command(string cmd, GUI::Widget *origin,
 	break;
 
     case BASH:
-    case MAKE:
     case PERL:
 	if (extra_data->refresh_pwd)
 	    cmds += gdb->pwd_command();
 	if (extra_data->refresh_breakpoints)
 	    cmds += "L";
+	if (extra_data->refresh_where)
+	    cmds += gdb->where_command();
+	if (extra_data->refresh_data)
+	    extra_data->n_refresh_data = 
+		data_disp->add_refresh_data_commands(cmds);
+	if (extra_data->refresh_user)
+	    extra_data->n_refresh_user = 
+		data_disp->add_refresh_user_commands(cmds);
+	if (extra_data->refresh_setting)
+	    cmds += show_command(cmd, gdb->type());
+	break;
+
+    case MAKE:
+	if (extra_data->refresh_pwd)
+	    cmds += gdb->pwd_command();
+	if (extra_data->refresh_breakpoints)
+	    cmds += "info breakpoints";
 	if (extra_data->refresh_where)
 	    cmds += gdb->where_command();
 	if (extra_data->refresh_data)
