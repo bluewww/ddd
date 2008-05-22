1cfaeed39611de031096aa7f7364e586d3fce5a6
pwainwright <pwainwright@1d73a310-8f1f-0410-b01a-dab0232e19a9>
2008-05-22 20:12:15 +0000

    Changed license from GPLv2 and LGPLv2 to GPLv3+ and LGPLv3+.

--- gtkx/GtkX/Toolbar.h
+++ gtkx/GtkX/Toolbar.h
@@ -8,7 +8,7 @@
 // GtkX is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
+// version 3 of the License, or (at your option) any later version.
 // 
 // GtkX is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,8 +17,7 @@
 // 
 // You should have received a copy of the GNU General Public
 // License along with GtkX -- see the file COPYING.
-// If not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// If not, see <http://www.gnu.org/licenses/>.
 
 // High level object-oriented wrapper must support Gtk and Motif.
 // Unfortunately Motif widgets require parent and name arguments to
