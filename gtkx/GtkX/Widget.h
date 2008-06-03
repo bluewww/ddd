2b6760c6016a1c6732f7a911da04735cfb6e3a34
pwainwright <pwainwright@1d73a310-8f1f-0410-b01a-dab0232e19a9>
2008-06-03 19:47:26 +0000

    Added more copyright notices and GPL.

--- gtkx/GtkX/Widget.h
+++ gtkx/GtkX/Widget.h
@@ -1,5 +1,32 @@
 // -*- C++ -*-
 
+// High-level GUI wrapper for Gtkmm.
+
+// Copyright (C) 2007 Peter Wainwright <prw@ceiriog.eclipse.co.uk>
+// 
+// This file is part of GtkX.
+// 
+// GtkX is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+// 
+// GtkX is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with GtkX -- see the file COPYING.
+// If not, see <http://www.gnu.org/licenses/>.
+
+// High level object-oriented wrapper must support Gtk and Motif.
+// Unfortunately Motif widgets require parent and name arguments to
+// the constructor, unlike the Gtk ones.  Motif (Xt) widgets cannot be
+// reparented.  Therefore we need a constructor with extra arguments.
+// A brief look at QT indicates that this will be required there as
+// well.
+
 #ifndef GTKX_WIDGET_H
 #define GTKX_WIDGET_H
 
