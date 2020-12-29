353b40aa5ee6f5b9492d6ab7dbce49b1dfacbcf4
bluewww <bluewww@users.noreply.github.com>
2020-12-29 23:28:02 +0100

    ci: Add packages and policy setting required for distcheck

--- .github/apt-deps.sh
+++ .github/apt-deps.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt update
-apt install libmotif-dev libxt-dev netpbm
+apt install libmotif-dev libxt-dev netpbm imagemagick texlive-font-utils fig2dev texlive-latex-base
+# allow imagemagick converts
+sed -i'/coder/d' /etc/ImageMagick-6/policy.xml
