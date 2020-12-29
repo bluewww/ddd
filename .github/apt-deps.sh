6c116e215434d0935427c2d3313ab317dd16f5a3
bluewww <bluewww@users.noreply.github.com>
2020-12-29 23:29:46 +0100

    ci: Enable distcheck and check targets

--- .github/apt-deps.sh
+++ .github/apt-deps.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 apt update
-apt install libmotif-dev libxt-dev netpbm imagemagick texlive-font-utils fig2dev texlive-latex-base
-# allow imagemagick converts
-sed -i'/coder/d' /etc/ImageMagick-6/policy.xml
+apt install libmotif-dev libxt-dev netpbm imagemagick texlive-font-utils texlive-latex-base
+
+if [[ $(lsb_release --release --short) == 16.04 ]]; then
+    apt install transfig
+else
+    apt install fig2dev
+fi
+
+# allow imagemagick converts by giving full permission
+sed -i '/coder/d' /etc/ImageMagick-6/policy.xml
+
+# HACK: we don't have pamarith on ubuntu (due to licensing problems afaik), but
+# pnmarith seems to work too
+ln -s /usr/bin/pnmarith /usr/bin/pamarith
+
